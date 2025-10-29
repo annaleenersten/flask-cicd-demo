f02c89c720b2afd43ecf5536d7334dcaf0c43732
annaleenersten <121985630+annaleenersten@users.noreply.github.com>
2025-10-29 12:44:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>🚀 Hello from Flask CI/CD Demo v2!</h1><p>Automatic Build → Test → Deploy via GitHub Actions.</p>"
+    return "<h1>🚀 Hello from Flask CI/CD Demo v2!</h1>"
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
